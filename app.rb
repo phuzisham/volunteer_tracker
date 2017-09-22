4ac9d98e62df9b658754435753544aad975cdcf3
David Hale <david.hale7@gmail.com>
2017-09-22 11:59:24 -0700

    first integration test passing

--- app.rb
+++ app.rb
@@ -3,4 +3,20 @@ require('sinatra/reloader')
 require('./lib/project')
 require('./lib/volunteer')
 require('pry')
+require('pg')
 also_reload('lib/**/*.rb')
+
+DB = PG.connect({:dbname => "volunteer_tracker_test"})
+
+get("/") do
+  @projects = Project.all()
+  erb(:index)
+end
+
+post('/project_input') do
+  title = params.fetch("title")
+  project = Project.new({:title => title, :id => nil})
+  project.save()
+  @projects = Project.all()
+  erb(:index)
+end
