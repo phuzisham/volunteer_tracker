71770d16c77055f9bc185baa1fb38eb92663c28a
David Hale <david.hale7@gmail.com>
2017-09-22 09:06:06 -0700

    project .all spec passing

--- lib/project.rb
+++ lib/project.rb
@@ -13,5 +13,16 @@ class Project
 
   def ==(another_project)
     self.title().==(another_project.title())
-end
+  end
+
+  def self.all
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each() do |project|
+      title = project.fetch("title")
+      id = project.fetch("id").to_i()
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+  projects
+  end
 end
