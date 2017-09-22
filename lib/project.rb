7a10b99137fd3db4a9ce381d723653899ba53b93
David Hale <david.hale7@gmail.com>
2017-09-22 08:51:10 -0700

    save spec working

--- lib/project.rb
+++ lib/project.rb
@@ -1,7 +1,13 @@
 class Project
   attr_reader(:title, :id)
+
   def initialize(attributes)
     @title = attributes.fetch(:title)
     @id = attributes.fetch(:id)
+  end
+
+  def save()
+    result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
   end
 end
